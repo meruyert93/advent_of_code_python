0091247c69116c062d654694550c07828b17a40e
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2023-12-01 00:40:18 +0100

    Update config.ini and add Day 01 puzzle files

---
+++ my_solutions/day_01/main.py
@@ -0,0 +1,14 @@
+import sys; sys.path.insert(0, '..')
+import aoc_lib as lib
+from pprint import pprint
+
+
+class DayPuzzleSolver():
+    def __init__(self):
+        self.delimiter = ""
+
+    def solve_part_1(self, raw_input):
+        ...
+
+    def solve_part_2(self, raw_input):
+        ...
