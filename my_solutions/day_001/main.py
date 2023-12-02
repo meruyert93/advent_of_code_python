371014e7e2eceaefe045f8acbde54edd2001016d
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2023-12-02 01:41:53 +0100

    initial commit

---
+++ my_solutions/day_001/main.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+import pprint
+from aoc_lib import ROW, COL
+import aoc_lib as lib
+import sys
+sys.path.insert(0, '..')
+
+
+class DayPuzzleSolver():
+    def __init__(self):
+        self.delimiter = "\n\n"
+
+    def solve_part_1(self, raw_input):
+        print("Part 1:")
+
+    def solve_part_2(self, raw_input):
+        ...
